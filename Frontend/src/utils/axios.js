175bc4c9e5bb629485028531f8d68a5d04251b59
Harsha <harsha446v@gmail.com>
2026-09-24 23:23:04 +0530

    Use same-origin API proxy for production

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -3,11 +3,8 @@
 import axios from 'axios';
 import qs from 'qs';
 
-const configuredApiUrl = import.meta.env.VITE_API_URL?.replace(/\/+$/, '');
-const apiBaseUrl = configuredApiUrl ? `${configuredApiUrl}/api` : '/api';
-
 export const axiosInstance = axios.create({
-    baseURL: apiBaseUrl,
+    baseURL: '/api',
     withCredentials: true,
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 })
